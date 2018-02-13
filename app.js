26d8ae9b17961e022e3cf21b80b24489471033f5
Christopher Lew <chrislew@lewcentral.com>
2018-02-13 11:27:04 -0500

    basic route complete

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+// app routes 
+// app.HTTPVerb(
+//   "path/being/visted",
+//   function middlewareCB (requestObject, responseObject) {
+//     // form server response
+//   }
+// );
+
+app.get('/', (req, res) => res.end('hey what is good?'));
+
+app.listen(3000, () => console.log('I am listening on port 3000'));
